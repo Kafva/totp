960ba32f9b6fc59740b22c33a38bbaa0d178cca1
Kafva <kafva.one@gmail.com>
2025-08-03 22:55:02 +0200

    First commit

---
+++ cli/src/main.rs
@@ -0,0 +1,34 @@
+use std::process::ExitCode;
+
+use clap::Parser;
+
+use totp::decode;
+
+#[derive(Parser)]
+#[command(version,
+    about = "Decoder for TOTP URIs",
+    long_about = None)]
+struct Args {
+    uri: Option<String>
+}
+
+fn main() -> ExitCode {
+    let args = Args::parse();
+
+    let uri = match args.uri {
+       Some(uri) => uri,
+       None => {
+            let mut buf = String::new();
+            let Ok(_) = std::io::stdin().read_line(&mut buf) else {
+                println!("Error reading input");
+                return ExitCode::FAILURE
+            };
+            buf
+       },
+    };
+
+    let code = decode(uri.as_str());
+    println!("{}", code);
+
+    ExitCode::SUCCESS
+}
---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! 
+//!
+//! https://www.rfc-editor.org/rfc/rfc6238.txt
+//!
+//! URL format:
+//!   otpauth://totp/<label>?secret=********************************&issuer=<issuer>
+
+pub fn decode(uri: &str) -> String {
+    uri.to_string()
+}
